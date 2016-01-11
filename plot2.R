1633c05d5e34e2576f4b8eabc66b1db166072da0
Kirsten Petersen <kirstenjpetersen@gmail.com>
2016-01-10 22:45:19 -0800

    Adding plot #2 script and png.

---
+++ plot2.R
@@ -0,0 +1,7 @@
+# Plot 2
+
+# Note: data was fetched and manipulated in plot1.R
+
+plot(mydf3$datetime,mydf3$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+dev.copy(png,"ExDataCP1Plot2.png")
+dev.off()
