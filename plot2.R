368144b33008fbe08a968f107346c2fe24bf56f9
Kirsten Petersen <kirstenjpetersen@gmail.com>
2016-01-10 23:37:12 -0800

    Added comment.

--- plot2.R
+++ plot2.R
@@ -3,5 +3,7 @@
 # Note: data was fetched and manipulated in plot1.R
 
 plot(mydf3$datetime,mydf3$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+
+# Write out to a file
 dev.copy(png,"ExDataCP1Plot2.png")
 dev.off()
